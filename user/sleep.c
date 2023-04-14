df660767aa8a9c009c37b86ca39e31f42cd3dcab
xkz0777 <xkz@mail.ustc.edu.cn>
2023-04-14 20:33:52 +0800

    finish sleep

---
+++ user/sleep.c
@@ -0,0 +1,17 @@
+#include "kernel/types.h" // strlen 的 uint 在这里声明
+#include "user/user.h" // syscall 和 ulib 的函数在这里声明
+
+#define STDIN 0
+#define STDOUT 1
+#define STDERR 2
+
+int main(int argc, char *argv[]) {
+    char *err_msg = "sleep: missing operand\nTry 'sleep --help' for more information.";
+    if (argc == 1) {
+        write(STDERR, err_msg, strlen(err_msg));
+        exit(-1);
+    }
+    int time = atoi(argv[2]);
+    sleep(time);
+    exit(0);
+}
