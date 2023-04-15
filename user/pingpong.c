57ab315da342bb9873f1bf413f4f387bdc01233c
xkz0777 <xkz@mail.ustc.edu.cn>
2023-04-15 17:17:59 +0800

    fix ping pong, finish lab util

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,27 +2,44 @@
 #include <user/user.h>
 #include <stddef.h>
 
-int main(int argc, char *argv[]) {
-    int fd[2];
+void Pipe(int *fd) {
     int r = pipe(fd);
     if (r < 0) {
         exit(-1);
     }
+}
+
+int main(int argc, char *argv[]) {
+    int parent_to_son[2];
+    int son_to_parent[2];
+    Pipe(parent_to_son);
+    Pipe(son_to_parent);
+    
     int pid = fork();
     if (pid == 0) {
         // 子进程
-        close(fd[1]);
-        char msg[2];
-        read(fd[0], msg, 1);
-        close(fd[0]);
+        close(parent_to_son[1]);
+        close(son_to_parent[0]);
+
+        char c;
+        read(parent_to_son[0], &c, 1);
+        close(parent_to_son[0]);
         fprintf(1, "%d: received ping\n", getpid());
+        
+        write(son_to_parent[1], &c, 1);
+        close(son_to_parent[1]);
         exit(0);
     } else {
-        close(fd[0]);
-        char *msg = "a";
-        write(fd[1], msg, 1);
-        close(fd[1]);
+        close(parent_to_son[0]);
+        close(son_to_parent[1]);
+
+        char msg = 'a';
+        write(parent_to_son[1], &msg, 1);
+        close(parent_to_son[1]);
+
         wait(NULL);
+        read(son_to_parent[0], &msg, 1);
+        close(son_to_parent[0]);
         fprintf(1, "%d: received pong\n", getpid());
         exit(0);
     }
