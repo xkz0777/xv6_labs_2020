30e4476e874d59e53b2d06a008542e686229209e
xkz0777 <821376043@qq.com>
2023-05-31 01:32:59 +0800

    finish lazy allocation

--- kernel/trap.c
+++ kernel/trap.c
@@ -68,9 +68,26 @@ usertrap(void)
   } else if((which_dev = devintr()) != 0){
     // ok
   } else {
-    printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
-    printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
-    p->killed = 1;
+    uint64 scause = r_scause();
+    if (scause == 13 || scause == 15) { // page fault
+      uint64 stval = r_stval();
+      uint64 va = PGROUNDDOWN(stval);
+      char *mem = kalloc();
+      if(mem == 0){
+        panic("memory exhaustion");
+      }
+      memset(mem, 0, PGSIZE);
+      if(mappages(p->pagetable, va, PGSIZE, (uint64)mem, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
+        kfree(mem);
+        uvmdealloc(p->pagetable, va, va);
+        panic("can't map");
+      }
+
+    } else {
+      printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
+      printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
+      p->killed = 1;
+    }
   }
 
   if(p->killed)
